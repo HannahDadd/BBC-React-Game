fe52260d563ae1efa4db73d42a714ba9885c6e35
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2018-10-04 16:33:11 +0100

    Draws board around Dalek

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       playerPos: {x: 1, y: 1},
       playerLocation: {x: 100, y: 100},
       movementCoefficent: 40,
+      boardDrawing: [],
       test: ""
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
@@ -30,7 +31,32 @@ class App extends Component {
     }
   }
 
-  // TODO Randomly generate the board
+
+  // Randomly generate the board
+  generateBoard(){
+    var width = 20;
+    var height = 20;
+    var newBoard = new Array(width);
+    var drawnBoard = [];
+
+    // Randomly generate the board with blocks to stop dalek in
+    for (var x = 0; x<width; x++){
+      newBoard[x] = [ ];
+      for (var y = 0; y<height; y++){
+        if (Math.floor((Math.random() * 10) + 1)=== 4){
+          newBoard[x][y] = 1;
+          drawnBoard.push(<div style={{width: this.state.movementCoefficent + "px", height: this.state.movementCoefficent + "px",
+              left: (100-this.state.movementCoefficent) + y*this.state.movementCoefficent + "px", 
+              top: (100-this.state.movementCoefficent) + x*this.state.movementCoefficent + "px", 
+              position: "absolute", border: "1px solid #000"}}></div>);
+        } else {
+          newBoard[x][y] = 0;
+        }
+      }
+    }
+  console.log(newBoard);
+  this.setState({board: newBoard, boardDrawing: drawnBoard});
+  }
 
   // TODO Randomly place the prizes
   
@@ -38,6 +64,7 @@ class App extends Component {
 
   // TODO add other players
 
+  // Move the player
   handleKeyDown(e) {
     this.setState({test: e.keyCode});
     // Up arrow pressed
@@ -69,25 +96,13 @@ class App extends Component {
 
 
   render() {
-    var board = [];
-    var i, j;
-    // Loop through each square in the board
-    for ( i = 0; i<this.state.board.length; i++){
-        for ( j = 0; j<this.state.board[0].length; j++){
-            if (this.state.board[i][j] === 1){
-              board.push(<div style={{width: this.state.movementCoefficent + "px", height: this.state.movementCoefficent + "px",
-                top: (100-this.state.movementCoefficent) + i *this.state.movementCoefficent + "px", 
-                left: (100-this.state.movementCoefficent) + j*this.state.movementCoefficent + "px", 
-                position: "absolute", border: "1px solid #000"}}></div>);
-            }
-        }
-    }
+    var game = this;
     return (
-      <div ref={this.playingArea} className="App" tabIndex="0" onKeyDown={this.handleKeyDown}>
-        <p>player x: {this.state.playerPos.x} player y: {this.state.playerPos.y} {}</p>
-        <p>{this.state.test}</p>
-        {board}
+      <div ref={this.playingArea} className="App" tabIndex="0" onClick={this.generateBoard.bind(game)} onKeyDown={this.handleKeyDown.bind(game)}>
+        <p>player x: {this.state.playerPos.x} player y: {this.state.playerPos.y}</p>
+        {this.state.boardDrawing}
         <img src={dalek} style={{position: "absolute", width: this.state.movementCoefficent, height: this.state.movementCoefficent, left: this.state.playerLocation.x + "px", top: this.state.playerLocation.y + "px"}}/>
+        
       </div>
     );
   }
