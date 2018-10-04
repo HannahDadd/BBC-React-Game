1da933e9338ca0e3ee448ba40e3636035af655eb
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2018-10-04 13:31:19 +0100

    Dalek player moves

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import dalek from './dalek.svg';
 
 class App extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class App extends Component {
     this.state = {
       board: [[1,0,0,1],[1,0,0,1],[1,0,0,1],[1,0,0,1]],
       playerPos: {x: 1, y: 1},
+      playerLocation: {x: 100, y: 100},
       test: ""
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
@@ -30,24 +32,28 @@ class App extends Component {
   handleKeyDown(e) {
     this.setState({test: e.keyCode});
     // Up arrow pressed
-    if (e.keyCode === 38) {
+    if (e.keyCode === 40) {
       if(this.state.playerPos.y < this.state.board.length -1 && this.canMove(this.state.playerPos.x, this.state.playerPos.y + 1)){
-        this.setState({playerPos: {x: this.state.playerPos.x, y: this.state.playerPos.y + 1}});
+        this.setState({playerPos: {x: this.state.playerPos.x, y: this.state.playerPos.y + 1}, 
+          playerLocation: {x: this.state.playerLocation.x, y: this.state.playerLocation.y + 100}});
       }
-    } else if (e.keyCode === 40) {
+    } else if (e.keyCode === 38) {
       // Down arrow pressed
       if(this.state.playerPos.y > 0 && this.canMove(this.state.playerPos.x, this.state.playerPos.y - 1)){
-        this.setState({playerPos: {x: this.state.playerPos.x, y: this.state.playerPos.y - 1}});
+        this.setState({playerPos: {x: this.state.playerPos.x, y: this.state.playerPos.y - 1}, 
+          playerLocation: {x: this.state.playerLocation.x, y: this.state.playerLocation.y - 100}});
       }
     } else if (e.keyCode === 39) {
       // Right arrow pressed
       if(this.state.playerPos.x < this.state.board[0].length -1 && this.canMove(this.state.playerPos.x + 1, this.state.playerPos.y)){
-        this.setState({playerPos: {x: this.state.playerPos.x + 1, y: this.state.playerPos.y}});
+        this.setState({playerPos: {x: this.state.playerPos.x + 1, y: this.state.playerPos.y}, 
+          playerLocation: {x: this.state.playerLocation.x + 100, y: this.state.playerLocation.y}});
       }
     } else if (e.keyCode === 37) {
       // Right arrow pressed
       if(this.state.playerPos.x > 0 && this.canMove(this.state.playerPos.x - 1, this.state.playerPos.y)){
-        this.setState({playerPos: {x: this.state.playerPos.x - 1, y: this.state.playerPos.y}});
+        this.setState({playerPos: {x: this.state.playerPos.x - 1, y: this.state.playerPos.y}, 
+          playerLocation: {x: this.state.playerLocation.x - 100, y: this.state.playerLocation.y}});
       }
     }
   }
@@ -58,6 +64,7 @@ class App extends Component {
       <div ref={this.playingArea} className="App" tabIndex="0" onKeyDown={this.handleKeyDown}>
         <p>player x: {this.state.playerPos.x} player y: {this.state.playerPos.y} {}</p>
         <p>{this.state.test}</p>
+        <img src={dalek} className="App-logo" style={{position: "absolute", left: this.state.playerLocation.x + "px", top: this.state.playerLocation.y + "px"}}/>
       </div>
     );
   }
